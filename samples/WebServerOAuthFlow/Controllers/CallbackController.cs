a9c70c328413050c812da60fc20849bc76f7e131
Wade Wegner <wade.wegner@gmail.com>
2014-01-10 10:37:40 -0700

    Added WebServerOAuthFlow sample

---
+++ samples/WebServerOAuthFlow/Controllers/CallbackController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Salesforce.Common;
+
+namespace WebServerOAuthFlow.Controllers
+{
+
+    public class CallbackController : ApiController
+    {
+        private readonly string _consumerKey = ConfigurationSettings.AppSettings["ConsumerKey"];
+        private readonly string _consumerSecret = ConfigurationSettings.AppSettings["ConsumerSecret"];
+        private readonly string _callbackUrl = ConfigurationSettings.AppSettings["CallbackUrl"];
+
+        public async Task<HttpResponseMessage> Get(string display, string code)
+        {
+            var auth = new AuthenticationClient();
+            await auth.WebServer(_consumerKey, _consumerSecret, _callbackUrl, code);
+
+            var url = string.Format("/?token={0}&api={1}&instance_url={2}", auth.AccessToken, auth.ApiVersion,
+                auth.InstanceUrl);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Redirect);
+            response.Headers.Location = new Uri(url, UriKind.Relative);
+
+            return response;
+        }
+
+        public HttpResponseMessage Get(string code)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.Redirect);
+            response.Headers.Location = new Uri("/", UriKind.Relative);
+
+            return response;
+        }
+    }
+}
---
+++ samples/WebServerOAuthFlow/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Salesforce.Common;
+
+namespace WebServerOAuthFlow.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _authorizationEndpointUrl = ConfigurationSettings.AppSettings["AuthorizationEndpointUrl"];
+        private readonly string _consumerKey = ConfigurationSettings.AppSettings["ConsumerKey"];
+        private readonly string _callbackUrl = ConfigurationSettings.AppSettings["CallbackUrl"];
+
+        public HomeController()
+        {
+            ViewBag.LoggedIn = false;
+        }
+
+        public ActionResult Index()
+        {
+            if (Request.QueryString.HasKeys())
+            {
+                var token = Request.QueryString["token"];
+                var apiVersion = Request.QueryString["api"];
+                var instanceUrl = Request.QueryString["instance_url"];
+
+                ViewBag.Token = token;
+                ViewBag.ApiVersion = apiVersion;
+                ViewBag.InstanceUrl = instanceUrl;
+
+                ViewBag.LoggedIn = true;
+            }
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var url =
+                Common.FormatAuthUrl(
+                    _authorizationEndpointUrl,
+                    ResponseTypes.Code,
+                    _consumerKey,
+                    HttpUtility.UrlEncode(_callbackUrl));
+
+            return Redirect(url);
+        }
+    }
+}
