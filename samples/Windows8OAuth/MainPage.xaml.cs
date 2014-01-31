c2eb21817d5748554caed297997ff68e962b9c19
Wade Wegner <wade.wegner@gmail.com>
2014-01-31 10:47:11 -0700

    Added a Windows 8 sample application

---
+++ samples/Windows8OAuth/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Salesforce.Common;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+namespace Windows8OAuth
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const string AuthorizationEndpointUrl = "https://login.salesforce.com/services/oauth2/authorize";
+        private const string ConsumerKey = "YOURCONSUMERKEY";
+        private const string CallbackUrl = "sfdc://success";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var startUrl = Common.FormatAuthUrl(AuthorizationEndpointUrl, ResponseTypes.Token, ConsumerKey, WebUtility.UrlEncode(CallbackUrl), DisplayTypes.Popup);
+            var startUri = new Uri(startUrl);
+            var endUri = new Uri(CallbackUrl);
+            string result;
+
+            try
+            {
+                var webAuthenticationResult =
+                    await Windows.Security.Authentication.Web.WebAuthenticationBroker.AuthenticateAsync(
+                    Windows.Security.Authentication.Web.WebAuthenticationOptions.None,
+                    startUri,
+                    endUri);
+
+                switch (webAuthenticationResult.ResponseStatus)
+                {
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.Success:
+
+                        var responseData = webAuthenticationResult.ResponseData;
+                        var responseUri = new Uri(responseData);
+                        var decoder = new WwwFormUrlDecoder(responseUri.Fragment.Replace("#", "?"));
+
+                        var accessToken = decoder.GetFirstValueByName("access_token");
+                        var refreshToken = decoder.GetFirstValueByName("refresh_token");
+                        var instanceUrl = WebUtility.UrlDecode(decoder.GetFirstValueByName("instance_url"));
+
+                        result = string.Format("Access Token: {0}\n\nRefresh Token: {1}\n\nInstance URL: {2}", accessToken,
+                            refreshToken, instanceUrl);
+
+                        break;
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.ErrorHttp:
+
+                        result = webAuthenticationResult.ResponseErrorDetail.ToString();
+                        break;
+
+                    default:
+
+                        result = webAuthenticationResult.ResponseData;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+
+            lblOutput.Text = result;
+        }
+    }
+}
--- samples/WindowsPhoneOAuth/AssociationUriMapper.cs
+++ samples/WindowsPhoneOAuth/AssociationUriMapper.cs
@@ -5,15 +5,20 @@ namespace WindowsPhoneOAuth
 {
     class AssociationUriMapper : UriMapperBase
     {
-        private string tempUri;
         public override Uri MapUri(Uri uri)
         {
-            tempUri = System.Net.HttpUtility.UrlDecode(uri.ToString());
-            
+            var tempUri = System.Net.HttpUtility.UrlDecode(uri.ToString());
+
             if (tempUri.Contains("sfdc://success"))
             {
-                var code = tempUri.Substring(tempUri.IndexOf("code=") + 5);
-                return new Uri("/MainPage.xaml?Token=" + code, UriKind.Relative);
+                var querystring = tempUri.Substring(tempUri.IndexOf("#") + 1);
+                var split = querystring.Split('&');
+                var accessToken = split[0].Split('=')[1];
+                var instanceUrl = split[2].Split('=')[1];
+
+                var url = string.Format("/MainPage.xaml?AccessToken={0}&InstanceUrl={1}", accessToken, instanceUrl);
+
+                return new Uri(url, UriKind.Relative);
             }
 
             return uri;
