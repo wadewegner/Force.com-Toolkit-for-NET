70269a852d863683778ca43dcbd7fbdf18a669c9
Wade Wegner <wade.wegner@gmail.com>
2014-02-05 09:31:27 -0700

    Updated Windows 8 sample to include token refresh

--- samples/Windows8OAuth/MainPage.xaml.cs
+++ samples/Windows8OAuth/MainPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Net;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows8OAuth.Models;
 using Salesforce.Common;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 namespace Windows8OAuth
 {
     /// <summary>
@@ -16,60 +17,84 @@ namespace Windows8OAuth
         private const string AuthorizationEndpointUrl = "https://login.salesforce.com/services/oauth2/authorize";
         private const string ConsumerKey = "YOURCONSUMERKEY";
         private const string CallbackUrl = "sfdc://success";
+        private Token _token;
 
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        async private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            var startUrl = Common.FormatAuthUrl(AuthorizationEndpointUrl, ResponseTypes.Token, ConsumerKey, WebUtility.UrlEncode(CallbackUrl), DisplayTypes.Popup);
-            var startUri = new Uri(startUrl);
-            var endUri = new Uri(CallbackUrl);
-            string result;
-
             try
             {
-                var webAuthenticationResult =
-                    await Windows.Security.Authentication.Web.WebAuthenticationBroker.AuthenticateAsync(
+                _token = await GetAccessToken();
+
+                var message = string.Format("User-Agent Auth Successful:\n\nAccess Token: {0}\n\nRefresh Token: {1}\n\nInstance URL: {2}",
+                    _token.AccessToken, _token.RefreshToken, _token.InstanceUrl);
+
+                lblOutput.Text = message;
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private async void btnRefreshToken_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var auth = new AuthenticationClient();
+                await auth.TokenRefreshAsync(ConsumerKey, _token.RefreshToken);
+
+                var message = string.Format("Token Refresh Successful:\n\nAccess Token: {0}\n\nInstance URL: {1}",
+                        auth.AccessToken, auth.InstanceUrl);
+
+                lblOutput.Text = message;
+            }
+            catch (ForceException ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private async Task<Token> GetAccessToken()
+        {
+            var token = new Token();
+
+            var startUrl = Common.FormatAuthUrl(AuthorizationEndpointUrl, ResponseTypes.Token, ConsumerKey,
+                WebUtility.UrlEncode(CallbackUrl), DisplayTypes.Popup);
+            var startUri = new Uri(startUrl);
+            var endUri = new Uri(CallbackUrl);
+
+            var webAuthenticationResult =
+                await Windows.Security.Authentication.Web.WebAuthenticationBroker.AuthenticateAsync(
                     Windows.Security.Authentication.Web.WebAuthenticationOptions.None,
                     startUri,
                     endUri);
 
-                switch (webAuthenticationResult.ResponseStatus)
-                {
-                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.Success:
-
-                        var responseData = webAuthenticationResult.ResponseData;
-                        var responseUri = new Uri(responseData);
-                        var decoder = new WwwFormUrlDecoder(responseUri.Fragment.Replace("#", "?"));
-
-                        var accessToken = decoder.GetFirstValueByName("access_token");
-                        var refreshToken = decoder.GetFirstValueByName("refresh_token");
-                        var instanceUrl = WebUtility.UrlDecode(decoder.GetFirstValueByName("instance_url"));
-
-                        result = string.Format("Access Token: {0}\n\nRefresh Token: {1}\n\nInstance URL: {2}", accessToken,
-                            refreshToken, instanceUrl);
-
-                        break;
-                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.ErrorHttp:
-
-                        result = webAuthenticationResult.ResponseErrorDetail.ToString();
-                        break;
-
-                    default:
-
-                        result = webAuthenticationResult.ResponseData;
-                        break;
-                }
-            }
-            catch (Exception ex)
+            switch (webAuthenticationResult.ResponseStatus)
             {
-                result = ex.Message;
-            }
+                case Windows.Security.Authentication.Web.WebAuthenticationStatus.Success:
+                    var responseData = webAuthenticationResult.ResponseData;
+                    var responseUri = new Uri(responseData);
+                    var decoder = new WwwFormUrlDecoder(responseUri.Fragment.Replace("#", "?"));
 
-            lblOutput.Text = result;
+                    token.AccessToken = decoder.GetFirstValueByName("access_token");
+                    token.RefreshToken = decoder.GetFirstValueByName("refresh_token");
+                    token.InstanceUrl = WebUtility.UrlDecode(decoder.GetFirstValueByName("instance_url"));
+
+                    return token;
+
+                case Windows.Security.Authentication.Web.WebAuthenticationStatus.ErrorHttp:
+                    throw new Exception(webAuthenticationResult.ResponseErrorDetail.ToString());
+
+                default:
+                    throw new Exception(webAuthenticationResult.ResponseData);
+            }
         }
+
+
     }
 }
