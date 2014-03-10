1857414948a836e57df8c51d7263acd9d168b26a
jason.rowe <jason.rowe@protolabs.com>
2014-03-10 15:18:22 -0500

    Changes to IForceClient for new method and making it public.
    Having the IForceClient interface public makes it easier to mock the ForceClient when testing.
    Adding in the new method that was added "UpsertExternalAsync".

--- src/ForceToolkitForNET/IForceClient.cs
+++ src/ForceToolkitForNET/IForceClient.cs
@@ -1,16 +1,16 @@
 ﻿//TODO: add license header
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Salesforce.Common.Models;
 
 namespace Salesforce.Force
 {
-    interface IForceClient
+    public interface IForceClient
     {
         Task<QueryResult<T>> QueryAsync<T>(string query);
         Task<string> CreateAsync(string objectName, object record);
         Task<bool> UpdateAsync(string objectName, string recordId, object record);
+        Task<bool> UpsertExternalAsync(string objectName, string externalId, string recordId, object record);
         Task<bool> DeleteAsync(string objectName, string recordId);
         Task<T> QueryByIdAsync<T>(string objectName, string recordId);
         Task<DescribeGlobalResult<T>> GetObjectsAsync<T>();
