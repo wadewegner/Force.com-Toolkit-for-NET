34425ab354333eb5c058d27bae07b62fe5d586aa
Wade Wegner <wade.wegner@gmail.com>
2014-03-24 12:28:28 -0600

    Fixed nunit.sh

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+mono --runtime=v4.0 nuget/buildsupport/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
 
 runTest(){
     mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
