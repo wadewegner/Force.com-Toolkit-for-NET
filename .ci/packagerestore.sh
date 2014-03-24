795ecb0b4e65f5484c82a6856f67057e67515879
Wade Wegner <wade.wegner@gmail.com>
2014-03-24 14:52:15 -0600

    Trying to restructure the script

--- .ci/packagerestore.sh
+++ .ci/packagerestore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 nuget/buildsupport/NuGet.exe restore -ConfigFile src/nuget.config -Verbosity detailed $@
+mono --runtime=v4.0 nuget/buildsupport/NuGet.exe restore $@ -ConfigFile src/nuget.config -Verbosity detailed -NoCache
 
 exit 0
