d9288b3dcfaca18bf2f192d4703a91f855d41fbf
Wade Wegner <wade.wegner@gmail.com>
2014-03-26 09:54:21 -0700

    Trying to disable parallel nuget to fix travis ci issues

--- .ci/packagerestore.sh
+++ .ci/packagerestore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 nuget/buildsupport/NuGet.exe restore $@ -ConfigFile src/NuGet.Config -Source nuget -Verbosity detailed -NoCache
+mono --runtime=v4.0 nuget/buildsupport/NuGet.exe restore $@ -ConfigFile src/NuGet.Config -Source nuget -Verbosity detailed -NoCache -DisableParallelProcessing
 
 exit 0
