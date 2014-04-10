adb7e45f500a35848b61c070e60d4afe14f2fc45
Wade Wegner <wade.wegner@gmail.com>
2014-04-10 13:20:38 -0600

    Added accelerator for web server oauth

---
+++ templates/WebServer/src/LoginController.cs.pp
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Salesforce.Common;
+using Salesforce.Common.Models;
+
+namespace $rootnamespace$.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly string _authorizationEndpointUrl = ConfigurationSettings.AppSettings["AuthorizationEndpointUrl"];
+        private readonly string _consumerKey = ConfigurationSettings.AppSettings["ConsumerKey"];
+        private readonly string _consumerSecret = ConfigurationSettings.AppSettings["ConsumerSecret"];
+        private readonly string _callbackUrl = ConfigurationSettings.AppSettings["CallbackUrl"];
+
+        public ActionResult Login()
+        {
+            var url =
+                Common.FormatAuthUrl(
+                    _authorizationEndpointUrl,
+                    ResponseTypes.Code,
+                    _consumerKey,
+                    HttpUtility.UrlEncode(_callbackUrl));
+
+            return Redirect(url);
+        }
+
+        public async Task<ActionResult> Callback(string display, string code)
+        {
+            var auth = new AuthenticationClient();
+            await auth.WebServerAsync(_consumerKey, _consumerSecret, _callbackUrl, code);
+
+            return Json(new { AccessToken = auth.AccessToken, ApiVersion = auth.ApiVersion, InstanceUrl = auth.InstanceUrl }, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
