02e6f2fb3a7319419b9a9f6dec7c0c3d51a34f1b
eweinstock-herman <eweinstock-herman@precisionlender.com>
2014-06-19 18:05:40 -0400

    Added functional test, added unit tests w/ known good content
    These new unit tests arefairly straightforward, but the objects shoudl allow for us to easily add things like auth error handling tests and such

---
+++ src/ForceToolkitForNet.UnitTests/DTO/ObjectFieldMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Salesforce.Force.UnitTests.DTO
+{
+    public class ObjectFieldMetadata
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+
+    }
+}
--- src/ForceToolkitForNet.UnitTests/ForceClientTests.cs
+++ src/ForceToolkitForNet.UnitTests/ForceClientTests.cs
@@ -4,6 +4,8 @@ using System;
 using System.Net.Http;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using System.Net;
+using Salesforce.Force.UnitTests.DTO;
 
 namespace Salesforce.Force.UnitTests
 {
@@ -25,5 +27,34 @@ namespace Salesforce.Force.UnitTests
            {
            }
         }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async void GetBasicInformationAsync_EmptyObjectName_ThrowsException()
+        {
+            var expectedResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            expectedResponse.Content = new JsonContent(new { });
+            var httpClient = new HttpClient(new FakeHttpRequestHandler(expectedResponse));
+            var forceClient = new ForceClient("http://localhost:1899", "accessToken", "v29", httpClient);
+
+            var result = await forceClient.GetBasicInformationAsync<object>("");
+
+            // expects exception
+        }
+
+        [Test]
+        public async void GetBasicInformationAsync_ValidObjectName_ReturnsParsedResponse()
+        {
+            var expectedResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            expectedResponse.Content = JsonContent.FromFile("KnownGoodContent/UserObjectDescribeMetadata.json");
+            var httpClient = new HttpClient(new FakeHttpRequestHandler(expectedResponse));
+            var forceClient = new ForceClient("http://localhost:1899", "accessToken", "v29", httpClient);
+
+            var result = await forceClient.GetBasicInformationAsync<ObjectDescribeMetadata>("ValidObjectName");
+
+            Assert.IsNotNullOrEmpty(result.Name);
+            Assert.AreEqual("User", result.Name);
+        }
+
     }
 }
