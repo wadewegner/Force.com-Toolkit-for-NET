ce029c9be5837de32f3a435a2cc9a5aa83c08384
Wade Wegner <wade.wegner@gmail.com>
2014-07-29 09:20:26 -0600

    Updated HttpPatchAsync to return id value when insert

---
+++ src/CommonLibrariesForNET.FunctionalTests/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesforce.Common.FunctionalTests.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ExternalId__c { get; set; }
+    }
+}
