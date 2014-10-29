77cfb88ecc4eef0bc73c1301d63cbb0f00f3cec3
Wade Wegner <wade.wegner@gmail.com>
2014-10-28 20:46:26 -0600

    Updated metadata for latest nuget push

--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -1,6 +1,6 @@
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.1.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.2.nupkg"
 
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.1.symbols.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.2.symbols.nupkg"
 
 "assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.8.nupkg"
 
