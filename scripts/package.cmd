df1a8ccd9f62ec893222e0a86c0a4077729e8d0b
Wade Wegner <wade.wegner@gmail.com>
2014-10-28 19:26:04 -0600

    Had to properly refactor for NuGet packages & symbols

--- scripts/package.cmd
+++ scripts/package.cmd
@@ -3,3 +3,7 @@ mkdir "../artifacts"
 "assets/NuGet.exe" pack "../src/ChatterToolkitForNET/ChatterToolkitForNET.nuspec" -Symbols -o "../artifacts"
 
 "assets/NuGet.exe" pack "../src/ForceToolkitForNET/ForceToolkitForNET.nuspec" -Symbols -o "../artifacts"
+
+"assets/NuGet.exe" pack "../src/ChatterToolkitForNET/ChatterToolkitForNET.Symbol.nuspec" -Symbols -o "../artifacts"
+
+"assets/NuGet.exe" pack "../src/ForceToolkitForNET/ForceToolkitForNET.Symbol.nuspec" -Symbols -o "../artifacts"
--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -1,3 +1,7 @@
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.0.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.1.nupkg"
 
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.7.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.0.6.1.symbols.nupkg"
+
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.8.nupkg"
+
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.8.symbols.nupkg"
