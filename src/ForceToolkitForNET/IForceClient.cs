12d52616e22081ae06c3aac76e0bf3decb8817a0
Wade Wegner <wade.wegner@gmail.com>
2015-01-26 13:42:34 -0700

    Updated IForceClient

--- src/ForceToolkitForNET/IForceClient.cs
+++ src/ForceToolkitForNET/IForceClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Salesforce.Common.Models;
 
 namespace Salesforce.Force
@@ -6,16 +7,22 @@ namespace Salesforce.Force
     public interface IForceClient
     {
         Task<QueryResult<T>> QueryAsync<T>(string query);
+        Task<QueryResult<T>> QueryContinuationAsync<T>(string nextRecordsUrl);
+        Task<QueryResult<T>> QueryAllAsync<T>(string query);
+        Task<T> QueryByIdAsync<T>(string objectName, string recordId);
         Task<string> CreateAsync(string objectName, object record);
         Task<SuccessResponse> UpdateAsync(string objectName, string recordId, object record);
-        Task<SuccessResponse> UpsertExternalAsync(string objectName, string externalId, string recordId, object record);
+        Task<SuccessResponse> UpsertExternalAsync(string objectName, string externalFieldName, string externalId, object record);
         Task<bool> DeleteAsync(string objectName, string recordId);
-        Task<T> QueryByIdAsync<T>(string objectName, string recordId);
         Task<DescribeGlobalResult<T>> GetObjectsAsync<T>();
         Task<T> BasicInformationAsync<T>(string objectName);
         Task<T> DescribeAsync<T>(string objectName);
+        Task<T> GetDeleted<T>(string objectName, DateTime startDateTime, DateTime endDateTime);
+        Task<T> GetUpdated<T>(string objectName, DateTime startDateTime, DateTime endDateTime);
         Task<T> DescribeLayoutAsync<T>(string objectName);
         Task<T> DescribeLayoutAsync<T>(string objectName, string recordTypeId);
-        Task<T> RecentAsync<T>(int limit);
+        Task<T> RecentAsync<T>(int limit = 200);
+        Task<T> UserInfo<T>(string url);
+        void Dispose();
     }
 }
