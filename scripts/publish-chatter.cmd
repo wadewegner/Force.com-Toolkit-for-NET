f9a66970f6301b0b3fe16a66ff5dcd7dd7146ab0
Wade Wegner <wade.wegner@gmail.com>
2015-01-27 12:30:04 -0700

    Updated for latest NuGet release

--- scripts/publish-chatter.cmd
+++ scripts/publish-chatter.cmd
@@ -1,3 +1,3 @@
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.8.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.9.nupkg"
 
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.8.symbols.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Chatter.0.4.9.symbols.nupkg"
