9cb89d7c638c68a09aeb9339664e41a16520eefe
Brad Slater <bslater@financialforce.com>
2015-08-13 15:54:03 +0100

    updated project files, added new methods to interface

--- src/CommonLibrariesForNET/ISoapServiceHttpClient.cs
+++ src/CommonLibrariesForNET/ISoapServiceHttpClient.cs
@@ -1,6 +1,9 @@
-﻿namespace Salesforce.Common
+﻿using System.Threading.Tasks;
+
+namespace Salesforce.Common
 {
     public interface ISoapServiceHttpClient
     {
+        Task<T> HttpPostAsync<T>(object inputObject, string urlSuffix);
     }
 }
--- src/ForceToolkitForNET/IForceBulkClient.cs
+++ src/ForceToolkitForNET/IForceBulkClient.cs
@@ -1,6 +1,10 @@
-﻿namespace Salesforce.Force
+﻿using System.Threading.Tasks;
+using Salesforce.Common.Models;
+
+namespace Salesforce.Force
 {
     public interface IForceBulkClient
     {
+        Task<JobInfoResult> CreateJobAsync(string objectName, ForceBulkClient.OperationType operationType);
     }
 }
