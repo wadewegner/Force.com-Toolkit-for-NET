3bd8c47d29c8f5b30b7e801ddf3f245c66ee8fac
Brad Slater <bslater@financialforce.com>
2015-08-14 17:57:23 +0100

    Created SObject and SObject list classes in order to be able to serialize and pass in dynamic dictionary objects.

---
+++ src/CommonLibrariesForNET/Models/Xml/SObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Salesforce.Common.Models.Xml
+{
+    public sealed class SObject : Dictionary<string,string>, IXmlSerializable
+    {
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            foreach (var entry in this)
+            {
+                writer.WriteRaw(string.Format("<{0}>{1}</{0}>", entry.Key, entry.Value));
+            }
+        }
+
+    }
+}
---
+++ src/CommonLibrariesForNET/Models/Xml/SObjectList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Salesforce.Common.Models.Xml
+{
+    [XmlRoot(ElementName="sObjects",
+             Namespace = "http://www.force.com/2009/06/asyncapi/dataload")]
+    public sealed class SObjectList : List<SObject>, IXmlSerializable
+    {
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            foreach (var entry in this)
+            {
+                writer.WriteRaw("<sObject>");
+                entry.WriteXml(writer);
+                writer.WriteRaw("</sObject>");
+            }
+        }
+    }
+}
