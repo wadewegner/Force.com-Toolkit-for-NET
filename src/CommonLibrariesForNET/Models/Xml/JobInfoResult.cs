23ca25b85edc3a01b082c373f72d3a191a134b56
Brad Slater <bslater@financialforce.com>
2015-08-14 15:58:46 +0100

    Removed unnecessary attribute

--- src/CommonLibrariesForNET/Models/Xml/JobInfoResult.cs
+++ src/CommonLibrariesForNET/Models/Xml/JobInfoResult.cs
@@ -3,9 +3,6 @@ using System.Xml.Serialization;
 
 namespace Salesforce.Common.Models.Xml
 {
-    [XmlRoot(Namespace = "http://www.force.com/2009/06/asyncapi/dataload",
-     ElementName = "jobInfo",
-     IsNullable = false)]
     public class JobInfoResult : JobInfo
     {
         [XmlElement(ElementName = "id")]
