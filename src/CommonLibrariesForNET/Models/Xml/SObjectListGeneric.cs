5de83bc602010fa2bff9fdedb2bbaceae0eb87b1
Brad Slater <bslater@financialforce.com>
2015-08-14 18:09:23 +0100

    Added the SObjectListGeneric helper class so you can use static object classes

---
+++ src/CommonLibrariesForNET/Models/Xml/SObjectListGeneric.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Salesforce.Common.Models.Xml
+{
+    [XmlRoot(ElementName = "sObjects",
+             Namespace = "http://www.force.com/2009/06/asyncapi/dataload")]
+    public class SObjectListGeneric<T> : List<T>
+    {
+    }
+}
