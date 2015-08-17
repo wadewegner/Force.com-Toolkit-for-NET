a5ab75bbd9cda1bbc2f60a5c445b5dc4bff192b0
Dave Carroll <djcarroll@gmail.com>
2015-08-17 13:20:50 -0700

    rollback to v34.0 api

--- src/CommonLibrariesForNET/AuthenticationClient.cs
+++ src/CommonLibrariesForNET/AuthenticationClient.cs
@@ -29,7 +29,7 @@ namespace Salesforce.Common
             if (httpClient == null) throw new ArgumentNullException("httpClient");
 
             _httpClient = httpClient;
-            ApiVersion = "v35.0";
+            ApiVersion = "v34.0";
         }
 
         public Task UsernamePasswordAsync(string clientId, string clientSecret, string username, string password)
