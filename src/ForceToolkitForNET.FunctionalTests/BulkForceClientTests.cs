178dffae2f36c3c2a298ad39d81000b0426ee142
Brad Slater <bslater@financialforce.com>
2015-08-19 15:56:28 +0100

    Revert "removed my test to see if it is breaking the current one"
    This reverts commit e4fd0819c36e14561a81913079c9eb3e586fcd71.

---
+++ src/ForceToolkitForNET.FunctionalTests/BulkForceClientTests.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using NUnit.Framework;
+using Salesforce.Common;
+using Salesforce.Force.Bulk;
+using Salesforce.Force.Bulk.Models;
+using Salesforce.Force.FunctionalTests.Models;
+
+namespace Salesforce.Force.FunctionalTests
+{
+    [TestFixture]
+    public class BulkForceClientTests
+    {
+        private static readonly string SecurityToken = ConfigurationManager.AppSettings["SecurityToken"];
+        private static readonly string ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
+        private static readonly string ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
+        private static readonly string Username = ConfigurationManager.AppSettings["Username"];
+        private static readonly string Password = ConfigurationManager.AppSettings["Password"] + SecurityToken;
+
+        private AuthenticationClient _auth;
+        private BulkForceClient _client;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            _auth = new AuthenticationClient();
+            _auth.UsernamePasswordAsync(ConsumerKey, ConsumerSecret, Username, Password).Wait();
+            _client = new BulkForceClient(_auth.InstanceUrl, _auth.AccessToken, _auth.ApiVersion);
+        }
+
+        [Test]
+        public async void FullRunThrough()
+        {
+            // Make a strongly typed Account list
+            var stAccountsBatch = new SObjectList<Account>
+            {
+                new Account {Name = "TestStAccount1"},
+                new Account {Name = "TestStAccount2"},
+                new Account {Name = "TestStAccount3"}
+            };
+
+            // insert the accounts
+            var results1 = await _client.RunJobAndPollAsync("Account", Bulk.Bulk.OperationType.Insert,
+                    new List<SObjectList<Account>> { stAccountsBatch });
+            // (one SObjectList<T> per batch, the example above uses one batch)
+
+            Assert.IsTrue(results1 != null);
+            Assert.AreEqual(results1.Count, 1);
+            Assert.AreEqual(results1[0].Count, 3);
+            Assert.IsTrue(results1[0][0].Created);
+            Assert.IsTrue(results1[0][0].Success);
+            Assert.IsTrue(results1[0][1].Created);
+            Assert.IsTrue(results1[0][1].Success);
+            Assert.IsTrue(results1[0][2].Created);
+            Assert.IsTrue(results1[0][2].Success);
+
+
+            // Make a dynamic typed Account list
+            var dtAccountsBatch = new SObjectList<SObject>
+            {
+                new SObject{{"Name", "TestDtAccount1"}},
+                new SObject{{"Name", "TestDtAccount2"}},
+                new SObject{{"Name", "TestDtAccount3"}}
+            };
+
+            // insert the accounts
+            var results2 = await _client.RunJobAndPollAsync("Account", Bulk.Bulk.OperationType.Insert,
+                    new List<SObjectList<SObject>> { dtAccountsBatch });
+
+            Assert.IsTrue(results2 != null);
+            Assert.AreEqual(results2.Count, 1);
+            Assert.AreEqual(results2[0].Count, 3);
+            Assert.IsTrue(results2[0][0].Created);
+            Assert.IsTrue(results2[0][0].Success);
+            Assert.IsTrue(results2[0][1].Created);
+            Assert.IsTrue(results2[0][1].Success);
+            Assert.IsTrue(results2[0][2].Created);
+            Assert.IsTrue(results2[0][2].Success);
+
+            // get the id of the first account created in the first batch
+            var id = results2[0][0].Id;
+            dtAccountsBatch = new SObjectList<SObject>
+            {
+                new SObject
+                {
+                    {"Id", id},
+                    {"Name", "TestDtAccount1Renamed"}
+                }
+            };
+
+            // update the first accounts name (dont really need bulk for this, just an example)
+            var results3 = await _client.RunJobAndPollAsync("Account", Bulk.Bulk.OperationType.Update,
+                    new List<SObjectList<SObject>> { dtAccountsBatch });
+
+            Assert.IsTrue(results3 != null);
+            Assert.AreEqual(results3.Count, 1);
+            Assert.AreEqual(results3[0].Count, 1);
+            Assert.AreEqual(results3[0][0].Id, id);
+            Assert.IsFalse(results3[0][0].Created);
+            Assert.IsTrue(results3[0][0].Success);
+
+            // create an Id list for the original strongly typed accounts created
+            var idBatch = new SObjectList<SObject>();
+            idBatch.AddRange(results1[0].Select(result => new SObject { { "Id", result.Id } }));
+
+            // delete all the strongly typed accounts
+            var results4 = await _client.RunJobAndPollAsync("Account", Bulk.Bulk.OperationType.Delete,
+                    new List<SObjectList<SObject>> { idBatch });
+
+            Assert.IsTrue(results4 != null);
+            Assert.AreEqual(results4.Count, 1);
+            Assert.AreEqual(results4[0].Count, 3);
+            Assert.IsFalse(results4[0][0].Created);
+            Assert.IsTrue(results4[0][0].Success);
+        }
+    }
+
+}
