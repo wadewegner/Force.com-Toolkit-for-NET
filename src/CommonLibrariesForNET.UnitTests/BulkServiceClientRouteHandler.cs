a02da2e2ae0cd5b36676a9bb3d7e9b4633055fb8
Brad Slater <bslater@financialforce.com>
2015-08-19 10:10:13 +0100

    Added the bulk service route handler

---
+++ src/CommonLibrariesForNET.UnitTests/BulkServiceClientRouteHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Salesforce.Common.UnitTests
+{
+    internal class BulkServiceClientRouteHandler : DelegatingHandler
+    {
+        readonly Action<HttpRequestMessage> _testingAction;
+        private readonly object _response;
+
+        public BulkServiceClientRouteHandler(Action<HttpRequestMessage> testingAction, object response)
+        {
+            _testingAction = testingAction;
+            _response = response;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
+        {
+            _testingAction(request);
+            var resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new XmlContent(_response)
+            };
+
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(resp);
+            return tsc.Task;
+        }
+    }
+}
