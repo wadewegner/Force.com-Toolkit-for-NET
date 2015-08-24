6669f03cefbfe86fa69accbc288bd38ad759e2c9
Justin Boyd <Justin@boyd.net.au>
2015-08-24 15:05:10 +1000

    fixing relative references

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,4 +1,4 @@
-"assets\NuGet.exe" restore "src\ForceToolkitForNET.sln" -ConfigFile "assets\NuGet.Config" -Source nuget -Verbosity detailed -NoCache -DisableParallelProcessing
+"scripts\assets\NuGet.exe" restore "src\ForceToolkitForNET.sln" -ConfigFile "assets\NuGet.Config" -Source nuget -Verbosity detailed -NoCache -DisableParallelProcessing
 
 "C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "src\ForceToolkitForNET.sln" /verbosity:minimal /target:Clean
 
