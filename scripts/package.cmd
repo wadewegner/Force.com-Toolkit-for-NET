a7dcd1da7521b7c5e5e9cb107b5e8dd92496db5c
Wade Wegner <wade.wegner@gmail.com>
2015-12-04 18:13:05 -0800

    Updated forNuGet

--- scripts/package.cmd
+++ scripts/package.cmd
@@ -2,8 +2,10 @@ mkdir "../artifacts"
 
 "assets/NuGet.exe" pack "assets/ChatterToolkitForNET.nuspec" -Symbols -o "../artifacts"
 
-"assets/NuGet.exe" pack "assets/ForceToolkitForNET.nuspec" -Symbols -o "../artifacts"
+"assets/NuGet.exe" pack "assets/ForceToolkitForNET.nuspec" -Symbols -o "../artifacts" 
 
 "assets/NuGet.exe" pack "assets/ChatterToolkitForNET.Symbol.nuspec" -Symbols -o "../artifacts"
 
 "assets/NuGet.exe" pack "assets/ForceToolkitForNET.Symbol.nuspec" -Symbols -o "../artifacts"
+
+
--- scripts/publish-force.cmd
+++ scripts/publish-force.cmd
@@ -1,3 +1,3 @@
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.1.2.1.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.1.2.2.nupkg"
 
-"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.1.2.1.symbols.nupkg"
+"assets/NuGet.exe" push "../artifacts/DeveloperForce.Force.1.2.2.symbols.nupkg"
