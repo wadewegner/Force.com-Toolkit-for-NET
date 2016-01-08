3e4fbe4d520c84cd251f62f4e86ecf911d6b757c
amccarter <adam@antidumb.com>
2016-01-08 10:27:43 -0600

    Update QueryResult.cs
    According to the API documentation (https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/quickstart_code.htm#get_soql_resource), the "done" field returns a boolean, not a string. This is causing the Json deserialization to fail.

--- src/CommonLibrariesForNET/Models/QueryResult.cs
+++ src/CommonLibrariesForNET/Models/QueryResult.cs
@@ -12,7 +12,7 @@ namespace Salesforce.Common.Models
         public int TotalSize { get; set; }
 
         [JsonProperty(PropertyName = "done")]
-        public string Done { get; set; }
+        public bool Done { get; set; }
 
         [JsonProperty(PropertyName = "records")]
         public List<T> Records { get; set; }
