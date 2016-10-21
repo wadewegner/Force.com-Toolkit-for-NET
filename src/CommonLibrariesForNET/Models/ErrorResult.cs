462c06beeb4afed5d67e67921151505be7c87fbc
Dave Humphreys <newky2k@icloud.com>
2016-10-21 08:58:03 +0100

    Added classes for the creating multiple records
    Added classes for the creating multiple records

---
+++ src/CommonLibrariesForNET/Models/ErrorResult.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesforce.Common.Models
+{
+    public class ErrorResult
+    {
+        [JsonProperty(PropertyName = "statusCode")]
+        public string StatusCode { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "fields")]
+        public List<string> Fields { get; set; }
+    }
+}
---
+++ src/CommonLibrariesForNET/Models/SaveResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesforce.Common.Models
+{
+    public class SaveResult
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName = "referenceId")]
+        public string ReferenceId { get; set; }
+
+        [JsonProperty(PropertyName = "errors")]
+        public List<Error> Errors { get; set; }
+    }
+
+}
