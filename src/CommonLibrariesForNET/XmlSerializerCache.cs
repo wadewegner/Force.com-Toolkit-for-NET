08a4fb9b6a6a195e830067cfe256e0f07a6e2a2d
Nick Rubino <rubino.nicholas@gmail.com>
2017-05-23 05:43:41 -0400

    Created an XmlSerializerCache to be used in the SObjectList class.  the XmlSerializer causes a serious memory leak if you do not use the two contructors mentioned here. https://support.microsoft.com/en-us/help/886385/memory-usage-is-high-when-you-create-several-xmlserializer-objects-in-asp-net
    From my testing this change also bumps performance atleast 2 fold with repetitive bulk saves

---
+++ src/CommonLibrariesForNET/XmlSerializerCache.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Salesforce.Common
+{
+
+    public class XmlSerializerCache
+    {
+        private static XmlSerializerCache _instance;
+
+        private XmlSerializerCache()
+        {
+            XmlSerializerDictionary = new Dictionary<string, XmlSerializer>();
+        }
+
+        public static XmlSerializerCache Instance => _instance ?? (_instance = new XmlSerializerCache());
+        public Dictionary<string, XmlSerializer> XmlSerializerDictionary { get; set; }
+    }
+}
