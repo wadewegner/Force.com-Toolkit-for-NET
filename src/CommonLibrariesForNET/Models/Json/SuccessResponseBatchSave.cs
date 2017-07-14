7ff6eceabed6e63b49a74742fd5797d3a1ac3fe8
Nick Rubino <rubino.nicholas@gmail.com>
2017-07-13 20:42:32 -0400

    SObjectTree and Batch composite functions available.

---
+++ src/CommonLibrariesForNET/Models/Json/SuccessResponseBatchSave.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Salesforce.Common.Models.Json
+{
+
+    public class SuccessResponseBatchSave
+    {
+        public bool hasErrors { get; set; }
+        public SuccessResponseBatchSave[] results { get; set; }
+    }
+
+    public class SuccessResponseBatchSaveResult
+    {
+        public int statusCode { get; set; }
+        public SuccessResponseBatchSaveResult1 result { get; set; }
+    }
+
+    public class SuccessResponseBatchSaveResult1
+    {
+        public SuccessResponseBatchSaveResult1Attributes attributes { get; set; }
+        public string Name { get; set; }
+        public string BillingPostalCode { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class SuccessResponseBatchSaveResult1Attributes
+    {
+        public string type { get; set; }
+        public string url { get; set; }
+    }
+
+
+}
+
