9d407d90845bdefef5170d9af9c7a5526cfb553b
James Kent <james.kent@vertic.com.au>
2017-09-11 16:21:53 +1000

    Expose all properties on IAuthenticationClient interface
    Cannot properly complete Dependency Injection missing Refresh Token.

--- src/CommonLibrariesForNET/IAuthenticationClient.cs
+++ src/CommonLibrariesForNET/IAuthenticationClient.cs
@@ -6,8 +6,11 @@ namespace Salesforce.Common
     public interface IAuthenticationClient : IDisposable
     {
         string InstanceUrl { get; set; }
+        string RefreshToken { get; set; }
         string AccessToken { get; set; }
+        string Id { get; set; }
         string ApiVersion { get; set; }
+        
         Task UsernamePasswordAsync(string clientId, string clientSecret, string username, string password);
         Task UsernamePasswordAsync(string clientId, string clientSecret, string username, string password, string tokenRequestEndpointUrl);
         Task WebServerAsync(string clientId, string clientSecret, string redirectUri, string code);
