225556256ce05f9e8db6935c5a5bdeda5307ae83
Wade Wegner <wade.wegner@gmail.com>
2018-08-20 14:25:28 -0700

    Fixed NuGet packaging

--- package.sh
+++ package.sh
@@ -1,5 +1,3 @@
-
-
 mkdir "artifacts"
 
 dotnet clean src/ForceToolkitForNET.sln
@@ -8,10 +6,6 @@ dotnet build src/ForceToolkitForNET.sln -c Release /p:DebugType=Full
 
 dotnet build src/ForceToolkitForNET.sln -c Debug /p:DebugType=Full
 
-dotnet pack "src/ForceToolkitForNET/ForceToolkitForNET.csproj" /p:NuspecFile="ForceToolkitForNET.nuspec" -o "../../artifacts"
+dotnet pack src/ForceToolkitForNET/ForceToolkitForNET.csproj -o "..\..\artifacts" --include-symbols --include-source
 
-dotnet pack "src/ForceToolkitForNET/ForceToolkitForNET.csproj" /p:NuspecFile="ForceToolkitForNET.Symbol.nuspec" -o "../../artifacts" --include-symbols  --include-source
-
-dotnet pack "src/ChatterToolkitForNET/ChatterToolkitForNET.csproj" /p:NuspecFile="ChatterToolkitForNET.nuspec" -o "../../artifacts"
-
-dotnet pack "src/ChatterToolkitForNET/ChatterToolkitForNET.csproj" /p:NuspecFile="ChatterToolkitForNET.Symbol.nuspec" -o "../../artifacts" --include-symbols  --include-source
+dotnet pack src/ChatterToolkitForNET/ChatterToolkitForNET.csproj -o "..\..\artifacts" --include-symbols --include-source
--- push.sh
+++ push.sh
@@ -1,7 +1,3 @@
-dotnet nuget push artifacts/DeveloperForce.Force.2.0.0.nupkg $1 -k $1 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/DeveloperForce.Force.$1.nupkg -k $2 -s https://api.nuget.org/v3/index.json
 
-# dotnet nuget push artifacts/DeveloperForce.Force.2.0.0.symbols.nupkg $1 -k $1 -s https://api.nuget.org/v3/index.json
-
-dotnet nuget push artifacts/DeveloperForce.Chatter.2.0.0.nupkg -k $1 -s https://api.nuget.org/v3/index.json
-
-# dotnet nuget push artifacts/DeveloperForce.Chatter.2.0.0.symbols.nupkg -k $1 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/DeveloperForce.Chatter.$1.nupkg -k $2 -s https://api.nuget.org/v3/index.json
