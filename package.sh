e25858b75e36bcfa515e609e86b4b310b9fb4e3b
Wade Wegner <wade.wegner@gmail.com>
2018-08-21 13:26:15 -0700

    Adding fixes for packaging

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
+rm -rf "artifacts"
 mkdir "artifacts"
 
 dotnet clean src/ForceToolkitForNET.sln
@@ -9,3 +10,15 @@ dotnet build src/ForceToolkitForNET.sln -c Debug /p:DebugType=Full
 dotnet pack src/ForceToolkitForNET/ForceToolkitForNET.csproj -o "..\..\artifacts" --include-symbols --include-source
 
 dotnet pack src/ChatterToolkitForNET/ChatterToolkitForNET.csproj -o "..\..\artifacts" --include-symbols --include-source
+
+cp artifacts/DeveloperForce.Chatter.$1.nupkg artifacts/DeveloperForce.Chatter.$1.nupkg.zip
+
+cp artifacts/DeveloperForce.Force.$1.nupkg artifacts/DeveloperForce.Force.$1.nupkg.zip
+
+unzip artifacts/DeveloperForce.Chatter.$1.nupkg.zip -d artifacts/DeveloperForce.Chatter
+
+unzip artifacts/DeveloperForce.Force.$1.nupkg.zip -d artifacts/DeveloperForce.Force
+
+chmod +r artifacts/DeveloperForce.Chatter/DeveloperForce.Chatter.nuspec
+
+chmod +r artifacts/DeveloperForce.Force/DeveloperForce.Force.nuspec
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-dotnet nuget push artifacts/DeveloperForce.Force.$1.nupkg -k $2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/DeveloperForce.Force.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push artifacts/DeveloperForce.Chatter.$1.nupkg -k $2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/DeveloperForce.Chatter.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
