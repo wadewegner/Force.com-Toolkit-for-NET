5cd9e2d30e66287efbc88003cce957cd1836a3bb
Wade Wegner <wade.wegner@gmail.com>
2018-08-22 15:47:41 -0700

    Fix for Travis build

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+rm -rf src/CommonLibrariesForNET/bin
+rm -rf src/CommonLibrariesForNET/obj
+rm -rf src/ChatterToolkitForNET/bin
+rm -rf src/ChatterToolkitForNET/obj
+rm -rf src/ForceToolkitForNET/bin
+rm -rf src/ForceToolkitForNET/obj
+
 dotnet clean src/ForceToolkitForNET.sln
 dotnet build src/ForceToolkitForNET.sln
